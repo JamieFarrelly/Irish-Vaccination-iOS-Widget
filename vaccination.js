e5c6bddc1d2398148451a47cf1f6ac8b291c5989
Jamie Farrelly <contact@jamiefarrelly.com>
2021-02-14 16:30:24 +0000

    Adding breakdown of first dose and second dose

--- vaccination.js
+++ vaccination.js
@@ -1,15 +1,31 @@
 // based on https://gist.github.com/planecore/e7b4c1e5db2dd28b1a023860e831355e
 
 const endpoint = "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/latest/owid-covid-latest.json"
-const country = "Ireland"
 const req = new Request(endpoint)
 const res = await req.loadJSON()
 
 if (config.runsInWidget) {
     // create and show widget
-    let widget = createWidget("Irish Vaccinations", `${res.IRL.total_vaccinations}`, ``, "#53d769")
-
-    Script.setWidget(widget)
+	let w = new ListWidget()
+    w.backgroundColor = new Color("#53d769")
+	
+    let titleTxt = w.addText(`Total Vaccinations: ${res.IRL.total_vaccinations}`)
+    titleTxt.textColor = Color.white()
+    titleTxt.font = Font.systemFont(14)
+    w.addSpacer(5)
+	
+    let subTxtFirstDose = w.addText(`First Dose: ${res.IRL.people_vaccinated}`)
+    subTxtFirstDose.textColor = Color.white()
+    subTxtFirstDose.textOpacity = 0.8
+    subTxtFirstDose.font = Font.systemFont(12)
+	w.addSpacer(2)
+	
+	let subTxtSecondDose = w.addText(`Fully Vaccinated: ${res.IRL.people_fully_vaccinated}`)
+    subTxtSecondDose.textColor = Color.white()
+    subTxtSecondDose.textOpacity = 0.8
+    subTxtSecondDose.font = Font.systemFont(12)
+	
+    Script.setWidget(w)
     Script.complete()
 } else {
     // make table
@@ -18,11 +34,13 @@ if (config.runsInWidget) {
     // add header
     let row = new UITableRow()
     row.isHeader = true
-    row.addText(`Vaccination Stats in ${country}`)
+    row.addText(`Vaccination in Ireland`)
     table.addRow(row)
 
     // fill data
-    table.addRow(createRow("People Vaccinated", res.IRL.total_vaccinations))
+	table.addRow(createRow("Total Vaccinations", res.IRL.total_vaccinations))
+    table.addRow(createRow("First Dose Vaccinations", res.IRL.people_vaccinated))
+	table.addRow(createRow("Fully Vaccinated", res.IRL.people_fully_vaccinated))
 
     // present table
     table.present()
@@ -33,23 +51,4 @@ function createRow(title, number) {
     row.addText(title)
     row.addText(number.toString()).rightAligned()
     return row
-}
-
-function createWidget(pretitle, title, subtitle, color) {
-    let w = new ListWidget()
-    w.backgroundColor = new Color(color)
-    let preTxt = w.addText(pretitle)
-    preTxt.textColor = Color.white()
-    preTxt.textOpacity = 0.8
-    preTxt.font = Font.systemFont(16)
-    w.addSpacer(5)
-    let titleTxt = w.addText(title)
-    titleTxt.textColor = Color.white()
-    titleTxt.font = Font.systemFont(22)
-    w.addSpacer(5)
-    let subTxt = w.addText(subtitle)
-    subTxt.textColor = Color.white()
-    subTxt.textOpacity = 0.8
-    subTxt.font = Font.systemFont(18)
-    return w
 }
